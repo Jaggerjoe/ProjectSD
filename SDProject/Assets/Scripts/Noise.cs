0ea1297dcfc5f477f744b55aea4dedba39d709bc
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-02-17 14:26:59 +0100

    scene

---
+++ SDProject/Assets/Scripts/Noise.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour
+{
+    [SerializeField] private float m_Damping = 0.2f;
+    [SerializeField] private float m_NoiseAmplitude = 0.5f;
+
+    private Transform m_Parent = null;
+    private Vector3 m_currentNoise = Vector3.zero;
+
+    private void Start()
+    {
+        m_Parent = transform.parent;
+        transform.parent = null;
+    }
+
+    private void Update()
+    {
+        float GetNoise(float x)
+        {
+            return Mathf.Lerp(-1f, 1f, Mathf.PerlinNoise(x, Time.time));
+        }
+
+        m_currentNoise.x = GetNoise(0f);
+        m_currentNoise.y = GetNoise(1f);
+        m_currentNoise.z = GetNoise(2f);
+        m_currentNoise *= m_NoiseAmplitude;
+
+        Vector3 targetPos = Vector3.Lerp(transform.position, m_Parent.transform.position + m_currentNoise, Time.deltaTime / m_Damping);
+        transform.position = targetPos;
+    }
+}
